6c223eb10e01132dd48c3dd46fcf40c40413c363
joakomedina <joaquinaarvelom@gmail.com>
2026-03-01 17:02:57 +0100

    ultima versión con propuesta de servicio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 export default {
-  darkMode: 'class',
+  darkMode: 'selector',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['"Inter"', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#18181b', // zinc-900
+          light: '#a1a1aa', // zinc-400
+        }
+      }
+    },
   },
   plugins: [],
 }
